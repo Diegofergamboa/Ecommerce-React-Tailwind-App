832c715f4782fb107b0084daa43e81e4a8a97e6d
Diegofergamboa <diegofergamboa@hotmail.es>
2023-05-06 15:31:18 -0500

    Install of React Routing

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import { useRoutes } from 'react-router-dom'
 import Home from '../Home/index'
 import MyAccount from '../MyAccount/index'
 import MyOrder from '../MyOrder/index'
 import MyOrders from '../MyOrders/index'
 import NotFound from '../NotFound/index'
 import SignIn from '../SignIn/index'
+import './App.css'
 
 function App() {
   return (
