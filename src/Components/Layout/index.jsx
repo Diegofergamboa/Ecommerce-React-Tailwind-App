9f0eeff66162097a9242f5e787fb1addae2a7b15
Diegofergamboa <diegofergamboa@hotmail.es>
2023-05-09 18:42:32 -0500

    Layout missing proptype fixed

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,9 +1,16 @@
-const Layout = ( {children} ) => {
+import PropTypes  from 'prop-types';
+
+const Layout = ({children}) => {
     return (
         <div className='flex flex-col items-center justify-center mt-20'>
             {children}
         </div>
     )
 }
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
 
 export default Layout
