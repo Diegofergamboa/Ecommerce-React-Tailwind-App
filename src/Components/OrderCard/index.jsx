9403d61631e3d56e9320c5291edd6095f338fcdb
diegofergamboa2 <diego.ortiz@onthefuze.com.au>
2023-05-26 17:24:29 -0500

    Fetching data to display in the context

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,5 +1,21 @@
-const OrderCard = () => {
+import { FaWindowClose } from 'react-icons/fa'
 
+const OrderCard = props => {
+    const { id, title, imageUrl, description, price } = props
+    return (
+        <div className="flex justify-between items-center">
+            <div className="flex items-center gap-2">
+                <figure className="w-20 h-20">
+                    <img className='w-full h-full rounded-lg object-cover' alt={description} src={imageUrl}/>
+                </figure>
+                <p className="text-sm font-light">{title}</p>
+            </div>
+            <div className="flex items-center gap-2">
+                <p className="text-lg font-medium">${price}</p>
+                <FaWindowClose className="h-6 text-black cursor-pointer" />
+            </div>
+        </div>
+    )
 }
 
 export default OrderCard;
