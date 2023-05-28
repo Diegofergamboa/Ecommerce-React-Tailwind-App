0770db0bfaa5c66f6a796265c83c6c5266ef3645
diegofergamboa2 <diego.ortiz@onthefuze.com.au>
2023-05-28 17:24:22 -0500

    Printing information in the MyOrder component

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -2,10 +2,10 @@ import './styles.css'
 import { FaWindowClose } from 'react-icons/fa'
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../Context'
-import { OrderCard } from '../../Components/OrderCard'
+import OrderCard from '../../Components/OrderCard'
 
 const CheckoutSideMenu = () => {
-    const { cartProducts, isCheckoutSideMenuActive, changeCheckoutSideMenu } = useContext(ShoppingCartContext)
+    const { cartProducts, isCheckoutSideMenuActive,changeCheckoutSideMenu } = useContext(ShoppingCartContext)
 
     return (
         <aside className={`${isCheckoutSideMenuActive ? 'flex ' : 'hidden '} checkout-side-menu rounded-lg flex flex-col fixed right-0 border bg-white border-black`}>
@@ -13,16 +13,16 @@ const CheckoutSideMenu = () => {
                 <FaWindowClose onClick={() => changeCheckoutSideMenu()} />
                 <h2 className='font-medium text-xl'>My Order</h2>
             </div>
-        {
-            cartProducts.map(product => {
-                <OrderCard 
-                key={product.Id}
-                title={product.title}
-                imageUrl={product.images}
-                price={product.price}
-                />
-            })
-        }
+            {
+                cartProducts.map(product => (
+                    <OrderCard
+                        key={product.Id}
+                        title={product.title}
+                        imageUrl={product.image}
+                        price={product.price}
+                    />
+                ))
+            }
         </aside>
     )
 }
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,12 +1,12 @@
-import { FaWindowClose } from 'react-icons/fa'
+import { FaWindowClose } from 'react-icons/fa';
 
 const OrderCard = props => {
-    const { id, title, imageUrl, description, price } = props
+    const { title, imageUrl, price } = props;
     return (
         <div className="flex justify-between items-center">
             <div className="flex items-center gap-2">
                 <figure className="w-20 h-20">
-                    <img className='w-full h-full rounded-lg object-cover' alt={description} src={imageUrl}/>
+                    <img className='w-full h-full rounded-lg object-cover' alt={title} src={imageUrl} />
                 </figure>
                 <p className="text-sm font-light">{title}</p>
             </div>
@@ -15,7 +15,7 @@ const OrderCard = props => {
                 <FaWindowClose className="h-6 text-black cursor-pointer" />
             </div>
         </div>
-    )
+    );
 }
 
 export default OrderCard;
