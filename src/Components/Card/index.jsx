2d8e934a32eff931069a5864f690d234f625e73f
diegofergamboa2 <diego.ortiz@onthefuze.com.au>
2023-06-01 15:16:03 -0500

    Bug on ad to order fixed

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../Context';
 import { FaCheck } from 'react-icons/fa'
+import { FaPlusCircle } from 'react-icons/fa'
 
 const Card = (data) => {
     const context = useContext(ShoppingCartContext)
@@ -19,20 +20,22 @@ const Card = (data) => {
         console.log('Cart', context.cartProducts)
     }
 
-    const renderIcon = (id) => {
-        const isInCart = context.cartProducts.filter(product => product.id === id).length > 0
-        if (isInCart) {
+    const renderIcon = (image) => {
+        console.log(`The image form the card is this ${image}`)
+        const isInCart = context.cartProducts.filter(product => product.image === image).length > 0   
+        console.log(isInCart)
+        if (!isInCart) {
             return (
-                <div className='absolute top-0 right-0 p-1 m-1 text-center align-middle flex justify-center items-center bg-white/80 w-6 h-6 rounded-full'
+                <div className='absolute top-0 right-0 p-1 m-1 text-center align-middle flex justify-center items-center w-6 h-6 bg-slate-50/80 shadow-lg font-bold rounded-full drop-shadow-2xl mix-blend-normal'
                     onClick={(event) => addShoppingCart(event, data)}
                 >
-                    +
+                    <FaPlusCircle />
                 </div>
             )
         }
         else {
-            (
-                <div className='absolute top-0 right-0 p-1 m-1 text-center align-middle flex justify-center items-center bg-white/80 w-6 h-6 rounded-full'
+            return (
+                <div className='absolute top-0 right-0 p-1 m-1 text-center align-middle flex justify-center items-center w-6 h-6 font-bold  bg-slate-50/80 shadow-lg drop-shadow-2xl rounded-full'
                     onClick={(event) => addShoppingCart(event, data)}
                 >
                     <FaCheck />
@@ -49,7 +52,8 @@ const Card = (data) => {
             <figure className='relative mb-2 w-full '>
                 <span className='absolute top-0 left-0 p-1 rounded-3xl m-1 bg-white/60 text-black text-sm '>{data.category}!</span>
                 <img className="rounded-lg object-cover" src={data.image} alt='headphones' />
-                {renderIcon(data.data.id)}
+                <p>{data.image}</p>
+                {renderIcon(data.image)}
             </figure>
             <p className='flex justify-between'>
                 <span className='text-sm font-bold'>{data.title}</span>
