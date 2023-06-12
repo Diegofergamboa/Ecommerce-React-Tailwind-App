b4358bcb71a25b40d42f08a46f8698910e1a3ed9
diegofergamboa2 <diego.ortiz@onthefuze.com.au>
2023-06-11 20:24:19 -0500

    Styles of the product detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -7,13 +7,13 @@ const ProductDetail = () => {
     const { changeProductDetail, isProductDetailActive, productToShow } = useContext(ShoppingCartContext)
 
     return (
-        <aside className={`${isProductDetailActive ? 'flex ' : 'hidden '}product-detail rounded-lg flex flex-col fixed right-0 border bg-white border-black`}>
+        <aside className={`${isProductDetailActive ? 'flex ' : 'hidden '} product-detail rounded-lg flex flex-col fixed right-0 border bg-white border-black`}>
             <div className='flex justify-between items-center p-6'>
                 <FaWindowClose onClick={() => changeProductDetail()} />
                 <h2 className='font-medium text-xl'>Product Information</h2>
             </div>
             <div className='flex flex-col p-6 gap-2 bg-gray-300 rounded-sm shadow-slate-500'>
-                <img src={productToShow.image} className='w-full  rounded-lg' />
+                <img src={productToShow.image} className='w-full  rounded-lg product-detail__image'/>
                 <h2 className='bold text-xl font-bold'>{productToShow.title}</h2>
                 <h4 className='font-bold text-lg underline'>${productToShow.price}</h4>
                 <p className='italic text-sm'>{productToShow.description}</p>
